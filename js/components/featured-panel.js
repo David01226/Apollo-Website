1db881ac86f299210ce38808f7e18c9067d3fcab
David Gibson <d.gibson1029384756@gmail.com>
2023-11-07 07:22:49 +0000

    created featured panel

---
+++ js/components/featured-panel.js
@@ -0,0 +1,43 @@
+class FeaturedPanel extends HTMLElement {
+  constructor() {
+    super();
+    this.sectionColor = this.getAttribute('sectionColor');
+    this.img = this.getAttribute('img')
+    this.imgPosition = this.getAttribute('imgPosition');
+    this.imgSize = this.getAttribute('imgSize');
+    this.sectionTitle = this.getAttribute('sectionTitle');
+    this.bodyText = this.getAttribute('bodyText');
+    this.btnStyle = this.getAttribute('btnStyle')
+    this.btn1Text = this.getAttribute('btn1Text');
+    this.btn1Link = this.getAttribute('btn1Link');
+    this.btn2Text = this.getAttribute('btn2Text');
+    this.btn2Link = this.getAttribute('btn2Link');
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="featured-panel ${this.imgPosition} ${this.imgSize} ${this.sectionColor}">
+    <div class="featured-panel__wrapper page-wrapper">
+      <div class="featured-panel__text-container">
+          <h2 class="featured-panel__title">${this.sectionTitle}</h2>
+          <p class="featured-panel__body-text">${this.bodyText}</p>
+
+        ${this.btn1Text ? 
+          `<div class="featured-panel__btn-container">
+          ${this.btn1Text ? `<a class="btn ${this.btnStyle}" href="${this.btn1Link}">${this.btn1Text}</a>` : ''}
+          ${this.btn2Text ? `<a class="btn ${this.btnStyle}" href="${this.btn2Link}">${this.btn2Text}</a>` : ''}
+          </div>`
+        :
+          ''
+        }
+      </div>
+      <div class="featured-panel__img-container">
+        <img src="${this.img}" alt="photo of team">
+      </div>
+      </div>
+    </div>
+    `;
+  }
+}
+
+customElements.define('featured-panel', FeaturedPanel);
--- js/components/quote.js
+++ js/components/quote.js
@@ -3,11 +3,12 @@ class Quote extends HTMLElement {
     super();
     this.quote = this.getAttribute('quote');
     this.author = this.getAttribute('author');
+    this.bgColor = this.getAttribute('bgColor');
   }
 
   connectedCallback() {
     this.innerHTML = `
-    <div class="quote">
+    <div class="quote ${this.bgColor}">
       <div class="quote__wrapper page-wrapper">
         <div class="quote__text">“${this.quote}”</div>
         <div class="quote__author">${this.author}</div>

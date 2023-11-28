bed7b0ddd520eb69d2a0e1a12b9202d579ce6881
David Gibson <d.gibson1029384756@gmail.com>
2023-11-28 21:39:23 +0000

    bug test fix

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,11 +1,14 @@
 let closeBtn = document.querySelector('.mobile-nav__close-container')
 let mobileMenu = document.querySelector('.mobile-nav__menu')
 let openBtn = document.querySelector('.mobile-nav__btn')
+let html = document.querySelector('html')
 
 closeBtn.addEventListener("click", () => {
   mobileMenu.style.left = "-100%";
+  html.style.overflowY = "unset";
 })
 
 openBtn.addEventListener("click", () => {
   mobileMenu.style.left = "0px";
+  html.style.overflowY = "hidden";
 })

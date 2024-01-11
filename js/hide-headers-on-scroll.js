2d4e4a7e32ed45ec72599e4b6507c19c4c987e0f
David Gibson <d.gibson1029384756@gmail.com>
2024-01-11 21:34:50 +0000

    added css animations and slide in and out on header

---
+++ js/hide-headers-on-scroll.js
@@ -0,0 +1,38 @@
+var lastScrollTop = 0;
+let pageWidth = window.innerWidth
+
+if (pageWidth > 1024) {
+  window.addEventListener("scroll", function(){ 
+     var st = window.pageYOffset || document.documentElement.scrollTop; 
+     let desktopHeader = document.querySelector('header');
+       if (lastScrollTop > 150) {
+         if (st > lastScrollTop) {
+           // downscroll code
+           console.log('scroll down')
+           desktopHeader.style.transform = 'translateY(-100%)';
+          } else if (st < lastScrollTop) {
+            // upscroll code
+            console.log('scroll down')
+            desktopHeader.style.transform = 'translateY(0%)';
+          } // else was horizontal scroll
+      }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  }, false);
+} else {
+  window.addEventListener("scroll", function(){ 
+     var st = window.pageYOffset || document.documentElement.scrollTop; 
+     let mobileHeader = document.querySelector('.mobile-nav');
+     if (lastScrollTop > 150) {
+         if (st > lastScrollTop) {
+           // downscroll code
+           console.log('scroll down')
+           mobileHeader.style.transform = 'translateY(-100%)';
+          } else if (st < lastScrollTop) {
+            // upscroll code
+            console.log('scroll down')
+            mobileHeader.style.transform = 'translateY(0%)';
+          } // else was horizontal scroll
+      }
+        lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+  }, false);
+}
